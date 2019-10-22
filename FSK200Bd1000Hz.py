96707db88a4d4c7fe497769ffb2e7fb03c24feea
pbusenius <patrick.busenius@gmail.com>
2019-10-22 22:13:24 +0200

    first!

---
+++ FSK200Bd1000Hz.py
@@ -0,0 +1,1 @@
+# TODO read wav file -> demodulation -> plot bits -> find frames -> decode and show result
---
+++ decoder.py
@@ -0,0 +1,7 @@
+import numpy as np
+
+
+class FSK200Bd1000Hz(object):
+    def __init__(self):
+        self.__sync_pattern = np.array([])
+
